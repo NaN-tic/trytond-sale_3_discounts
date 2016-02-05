ce183247f62e2edf4b4f6582c790abe6cf20f332
Albert Cervera i Areny <albert@nan-tic.com>
2016-02-06 00:24:50 +0100

    Initial commit.

---
+++ invoice.py
@@ -0,0 +1,69 @@
+from trytond.model import fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
+from trytond.config import config
+DISCOUNT_DIGITS = int(config.get('digits', 'discount_digits', 4))
+
+__all__ = ['InvoiceLine']
+
+STATES = {
+    'invisible': Eval('type') != 'line',
+    'required': Eval('type') == 'line',
+    }
+DEPENDS = ['type']
+
+
+class InvoiceLine:
+    __name__ = 'account.invoice.line'
+    __metaclass__ = PoolMeta
+    discount1 = fields.Numeric('Discount 1', digits=(16, DISCOUNT_DIGITS),
+        states=STATES, depends=DEPENDS)
+    discount2 = fields.Numeric('Discount 2', digits=(16, DISCOUNT_DIGITS),
+        states=STATES, depends=DEPENDS)
+    discount3 = fields.Numeric('Discount 3', digits=(16, DISCOUNT_DIGITS),
+        states=STATES, depends=DEPENDS)
+
+    @classmethod
+    def __setup__(cls):
+        super(InvoiceLine, cls).__setup__()
+        discounts = set(['discount1', 'discount2', 'discount3'])
+        cls.amount.on_change_with |= discounts
+        cls.product.on_change |= discounts
+        cls.gross_unit_price.on_change |= discounts
+        cls.discount.on_change |= discounts
+
+    @staticmethod
+    def default_discount1():
+        return 0
+
+    @staticmethod
+    def default_discount2():
+        return 0
+
+    @staticmethod
+    def default_discount3():
+        return 0
+
+    def update_prices(self):
+        discount1 = self.discount1 or Decimal(0)
+        discount2 = self.discount2 or Decimal(0)
+        discount3 = self.discount3 or Decimal(0)
+        self.discount = 1 - ((1 - discount1) * (1 - discount2) * (1 -
+                discount3))
+        digits = self.__class__.discount.digits[1]                   
+        self.discount = self.discount.quantize(Decimal(str(10.0 ** -digits)))
+        res = super(InvoiceLine, self).update_prices()
+        res['discount'] = self.discount
+        return res
+
+    @fields.depends('discount1', 'discount2', 'discount3', methods=['discount'])
+    def on_change_discount1(self):
+        return self.update_prices()
+
+    @fields.depends('discount1', 'discount2', 'discount3', methods=['discount'])
+    def on_change_discount2(self):
+        return self.update_prices()
+
+    @fields.depends('discount1', 'discount2', 'discount3', methods=['discount'])
+    def on_change_discount3(self):
+        return self.update_prices()
---
+++ sale.py
@@ -0,0 +1,90 @@
+# This file is part of sale_discount module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from decimal import Decimal
+from trytond.model import fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
+from trytond.config import config
+DIGITS = int(config.get('digits', 'unit_price_digits', 4))
+DISCOUNT_DIGITS = int(config.get('digits', 'discount_digits', 4))
+_ZERO = Decimal(0)
+
+__all__ = ['SaleLine']
+__metaclass__ = PoolMeta
+
+STATES = {
+    'invisible': Eval('type') != 'line',
+    'required': Eval('type') == 'line',
+    }
+DEPENDS = ['type']
+
+
+class SaleLine:
+    __name__ = 'sale.line'
+    __metaclass__ = PoolMeta
+    discount1 = fields.Numeric('Discount 1', digits=(16, DISCOUNT_DIGITS),
+        states=STATES, depends=DEPENDS)
+    discount2 = fields.Numeric('Discount 2', digits=(16, DISCOUNT_DIGITS),
+        states=STATES, depends=DEPENDS)
+    discount3 = fields.Numeric('Discount 3', digits=(16, DISCOUNT_DIGITS),
+        states=STATES, depends=DEPENDS)
+
+    @classmethod
+    def __setup__(cls):
+        super(SaleLine, cls).__setup__()
+        discounts = set(['discount1', 'discount2', 'discount3'])
+        cls.amount.on_change_with |= discounts
+        cls.product.on_change |= discounts
+        cls.quantity.on_change |= discounts
+        cls.gross_unit_price.on_change |= discounts
+        cls.discount.on_change |= discounts
+        if hasattr(cls, 'package_quantity'):
+            cls.package_quantity.on_change |= discounts
+
+    @staticmethod
+    def default_discount1():
+        return 0
+
+    @staticmethod
+    def default_discount2():
+        return 0
+
+    @staticmethod
+    def default_discount3():
+        return 0
+
+    def update_prices(self):
+        discount1 = self.discount1 or _ZERO
+        discount2 = self.discount2 or _ZERO
+        discount3 = self.discount3 or _ZERO
+        self.discount = 1 - ((1 - discount1) * (1 - discount2) * (1 -
+                discount3))
+        digits = self.__class__.discount.digits[1]
+        self.discount = self.discount.quantize(Decimal(str(10.0 ** -digits)))
+        res = super(SaleLine, self).update_prices()
+        res['discount'] = self.discount
+        return res
+
+    @fields.depends('discount1', 'discount2', 'discount3',
+        '_parent_sale.sale_discount', methods=['discount'])
+    def on_change_discount1(self):
+        return self.update_prices()
+
+    @fields.depends('discount1', 'discount2', 'discount3',
+        '_parent_sale.sale_discount', methods=['discount'])
+    def on_change_discount2(self):
+        return self.update_prices()
+
+    @fields.depends('discount1', 'discount2', 'discount3',
+        '_parent_sale.sale_discount', methods=['discount'])
+    def on_change_discount3(self):
+        return self.update_prices()
+
+    def get_invoice_line(self, invoice_type):
+        lines = super(SaleLine, self).get_invoice_line(invoice_type)
+        for line in lines:
+            line.discount1 = self.discount1
+            line.discount2 = self.discount2
+            line.discount3 = self.discount3
+        return lines
